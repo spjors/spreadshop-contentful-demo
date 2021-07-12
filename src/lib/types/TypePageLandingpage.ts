464ce77f20cd0daa6929189231da846dfc2d8e8a
Martin Breest <mbs@spreadshirt.net>
2021-07-12 12:54:08 +0200

    Removed TypeSection from source.

--- src/lib/types/TypePageLandingpage.ts
+++ src/lib/types/TypePageLandingpage.ts
@@ -1,20 +1,30 @@
-import * as Contentful from "contentful";
-import { TypeBannerFields } from "./TypeBanner";
-import { TypeBlogRollFields } from "./TypeBlogRoll";
-import { TypeCalculatorFields } from "./TypeCalculator";
-import { TypeCarouselFields } from "./TypeCarousel";
-import { TypeFaqFields } from "./TypeFaq";
-import { TypeFeaturedUspFields } from "./TypeFeaturedUsp";
-import { TypeHeroImageFields } from "./TypeHeroImage";
-import { TypeSectionFields } from "./TypeSection";
-import { TypeSingleAssetFields } from "./TypeSingleAsset";
-import { TypeTestimonialFields } from "./TypeTestimonial";
-import { TypeUspListFields } from "./TypeUspList";
+import * as Contentful from 'contentful';
+import { TypeBannerFields } from './TypeBanner';
+import { TypeBlogRollFields } from './TypeBlogRoll';
+import { TypeCalculatorFields } from './TypeCalculator';
+import { TypeCarouselFields } from './TypeCarousel';
+import { TypeFaqFields } from './TypeFaq';
+import { TypeFeaturedUspFields } from './TypeFeaturedUsp';
+import { TypeHeroImageFields } from './TypeHeroImage';
+import { TypeSingleAssetFields } from './TypeSingleAsset';
+import { TypeTestimonialFields } from './TypeTestimonial';
+import { TypeUspListFields } from './TypeUspList';
 
 export interface TypePageLandingpageFields {
-    name?: Contentful.EntryFields.Symbol;
-    hero: Contentful.Entry<TypeHeroImageFields>;
-    sections?: Contentful.Entry<TypeBannerFields | TypeBlogRollFields | TypeCalculatorFields | TypeCarouselFields | TypeFaqFields | TypeFeaturedUspFields | TypeHeroImageFields | TypeSectionFields | TypeSingleAssetFields | TypeTestimonialFields | TypeUspListFields>[];
+  name?: Contentful.EntryFields.Symbol;
+  hero: Contentful.Entry<TypeHeroImageFields>;
+  sections?: Contentful.Entry<
+    | TypeBannerFields
+    | TypeBlogRollFields
+    | TypeCalculatorFields
+    | TypeCarouselFields
+    | TypeFaqFields
+    | TypeFeaturedUspFields
+    | TypeHeroImageFields
+    | TypeSingleAssetFields
+    | TypeTestimonialFields
+    | TypeUspListFields
+  >[];
 }
 
 export type TypePageLandingpage = Contentful.Entry<TypePageLandingpageFields>;
